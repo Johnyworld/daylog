b5bc7a67e43e21a25c56fa1d5cd29d56bcdbf1a5
Johny Kim <johnyworld@naver.com>
2019-09-17 18:31:36 +0900

    Set : Prisma setting done

---
+++ src/api/Hello/Hello.js
@@ -0,0 +1,10 @@
+import { prisma } from '../../../generated/prisma-client';
+
+export default {
+    Query : {
+        hello: async() => {
+            console.log( await prisma.users() );
+            return "hello";
+        } 
+    }
+};
