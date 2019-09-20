63d39c28a440f73d8b9bcdf79b58218bb817006a
Johny Kim <johnyworld@naver.com>
2019-09-20 18:36:44 +0900

    Add : Doing's functions
    Count followers
    Count posts
    Count blocks
    Check logged user is following

--- src/api/Doing/Doing.js
+++ src/api/Doing/Doing.js
@@ -4,6 +4,39 @@ export default {
     Doing : {
         category: ({ id }) => prisma.doing({ id }).category(),
         followers: ({ id }) => prisma.doing({ id }).followers(),
-        posts: ({ id }) => prisma.doing({ id }).posts()
+        posts: ({ id }) => prisma.doing({ id }).posts(),
+
+        followersCount: ({ id }) => prisma.usersConnection({
+            where: { doings_some: { id } }
+        }).aggregate().count(),
+
+        postsCount: ({ id }) => prisma.postsConnection({
+            where: { doing : { id } }
+        }).aggregate().count(),
+
+        blocksCount: async({ id }) => {
+            let total = 0;
+
+            const posts = await prisma.postsConnection({
+                where: { doing: { id } }
+            }).edges();
+
+            posts.map( post => {
+                const startAt = post.node.startAt;
+                const endAt = post.node.endAt;
+                if ( startAt && endAt ) {
+                    total += endAt - startAt;
+                }
+            });
+            return total;
+        },
+
+        isFollowing: ({ id }, _, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return prisma.$exists.user({
+                id: user.id, doings_some: { id }
+            })
+        }
     }
 }
