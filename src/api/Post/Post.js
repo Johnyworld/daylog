8b719bcba28e818e6fbc89f1acee04e0ae11896c
Johny Kim <johnyworld@naver.com>
2019-09-21 00:09:36 +0900

    Add : Post's function
    Count likes
    Count comments
    Check logged user liked this post
    Timeblocks

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -3,6 +3,29 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     Post : {
         user: ({ id }) => prisma.post({ id }).user(),
-        doing: ({ id }) => prisma.post({ id }).doing()
+        doing: ({ id }) => prisma.post({ id }).doing(),
+
+        likesCount: ({ id }) => prisma.likesConnection({
+            where: { post: { id }}
+        }).aggregate().count(),
+
+        commentsCount: ({ id }) => prisma.commentsConnection({
+            where: { post: { id }}
+        }).aggregate().count(),
+        
+        blocks: ({ startAt, endAt }) => {
+            return endAt - startAt;
+        },
+
+        isLiked: ({ id }, _, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return prisma.$exists.like({
+                AND : [
+                    { post: { id }},
+                    { user: { id: user.id }}
+                ]
+            })
+        }
     }
 }
