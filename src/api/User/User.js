4e7fe05a757cbeb6d7e0a950da196c578dc587b1
Johny Kim <johnyworld@naver.com>
2019-09-21 00:12:13 +0900

    Add : User's functions
    Count posts
    Count followers
    Count following
    Count total likes
    Check logged user self
    Check logged user is following

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -9,5 +9,54 @@ export default {
         doings: ({ id }) => prisma.user({ id }).doings(),
         comments: ({ id }) => prisma.user({ id }).comments(),
         reviews: ({ id }) => prisma.user({ id }).reviews(),
+
+        postsCount: ({ id }) => prisma.postsConnection({
+            where: { user: { id } }
+        }).aggregate().count(),
+
+        followersCount: ({ id }) => prisma.usersConnection({
+            where: { following_some: { id } }
+        }).aggregate().count(),
+
+        followingCount: ({ id }) => prisma.usersConnection({
+            where: { followers_some: { id } }
+        }).aggregate().count(),
+
+        likesTotal: async({ id }) => {
+            const likesOwnComments = await prisma.likesConnection({
+                where: { post : { user : { id } } }
+            }).aggregate().count();
+
+            const likesOwnReviews = await prisma.likesConnection({
+                where: { review : { user : { id } } }
+            }).aggregate().count();
+
+            const didLikes = await prisma.likesConnection({
+                where: { user : { id }}
+            }).aggregate().count();
+
+            return likesOwnComments + likesOwnReviews + didLikes;
+        },
+        
+        isFollowing: async({ id }, __, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user } = request;
+            try {
+                return prisma.$exists.user({
+                    AND : [
+                        { id },
+                        { followers_some: { id: user.id } }
+                    ]
+                });
+            } catch {
+                return false;
+            }
+        },
+
+        isSelf: async({ id }, _, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return user.id === id; 
+        }
     }
 }
