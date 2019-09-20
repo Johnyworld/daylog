51e70d9da2a7a7a883df254aedf33c63ecc8cb08
Johny Kim <johnyworld@naver.com>
2019-09-20 19:01:26 +0900

    Add : Review's function
    Count likes

--- src/api/Review/Review.js
+++ src/api/Review/Review.js
@@ -2,6 +2,10 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
     Review : {
-        user: ({ id }) => prisma.review({ id }).user()
+        user: ({ id }) => prisma.review({ id }).user(),
+
+        likesCount: ({ id }) => prisma.likesConnection({
+            where: { review : { id } }
+        }).aggregate().count()
     }
 }
