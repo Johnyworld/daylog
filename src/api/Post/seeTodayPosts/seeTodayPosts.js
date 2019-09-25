59b7c681618739a803bd0818c9e466265c73eed1
Johny Kim <johnyworld@naver.com>
2019-09-25 13:49:23 +0900

    Add : Show Today posts

---
+++ src/api/Post/seeTodayPosts/seeTodayPosts.js
@@ -0,0 +1,19 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { getYyyymmdd } from "../../../utils";
+
+export default {
+    Query : {
+        seeTodayPosts : (_, __, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user : { username } } = request;
+            const yyyymmdd = getYyyymmdd( new Date().getFullYear(), new Date().getMonth(), new Date().getDate() );
+            console.log(yyyymmdd)
+            return prisma.posts({
+                where : {
+                    user: { username },
+                    yyyymmdd
+                }
+            })
+        }
+    }
+}
