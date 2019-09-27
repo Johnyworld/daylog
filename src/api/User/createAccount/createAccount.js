162e4f26e1bdb4efa50cd236b36b80660af65b8e
Johny Kim <johnyworld@naver.com>
2019-09-27 16:32:42 +0900

    Update : Avatar default image deleted

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,10 @@ export default {
     Mutation : {
         createAccount : async(_, args) => {
             const { username, email, fullname="", bio="" } = args;
+
+            const regUsername = /^[a-z0-9_-]{3,16}$/; 
+            if ( !regUsername.test( username ) ) throw Error("You can make username by 3-15 characters, lower case, number, -, _");
+
             const exists = await prisma.$exists.user({ OR: [ {username}, {email} ] });
             if ( exists ) {
                 throw Error("This username or email is already taken.")
