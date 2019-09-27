78dbce8aa71bfcb57c4cf6addf7ec90b970b7975
Johny Kim <johnyworld@naver.com>
2019-09-27 13:55:57 +0900

    Update : Delete ACTION from edit comment API

--- src/api/Comment/editComment/editComment.js
+++ src/api/Comment/editComment/editComment.js
@@ -1,8 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
 
-const EDIT = "EDIT";
-const DELETE = "DELETE";
-
 export default {
     Mutation : {
         editComment: async(_, args, {request, isAuthenticated}) => {
@@ -11,8 +8,9 @@ export default {
             const { user } = request;
             const post = await prisma.$exists.comment({ id, user : { id : user.id } });
             if ( post ) {
-                if ( text && action === EDIT ) return prisma.updateComment({ where: { id }, data: { text } });
-                else if  ( action === DELETE ) return prisma.deleteComment({ id });
+                if ( text && action === "EDIT" ) await prisma.updateComment({ where: { id }, data: { text } });
+                else if  ( action === "DELETE" ) await prisma.deleteComment({ id });
+                return true;
             } else {
                 throw Error("This comment is not yours")
             }
