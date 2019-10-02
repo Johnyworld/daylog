dc71951b769f4918ab4d0600744dddabb6d68dcb
Johny Kim <johnyworld@naver.com>
2019-10-02 16:50:20 +0900

    Add isLiked on reviews

--- src/api/Review/Review.js
+++ src/api/Review/Review.js
@@ -6,6 +6,17 @@ export default {
 
         likesCount: ({ id }) => prisma.likesConnection({
             where: { review : { id } }
-        }).aggregate().count()
+        }).aggregate().count(),
+
+        isLiked: ({ id }, _, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return prisma.$exists.like({
+                AND : [
+                    { review: { id }},
+                    { user: { id: user.id }}
+                ]
+            })
+        }
     }
 }
