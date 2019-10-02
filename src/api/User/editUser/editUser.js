b0786307bb7acdf854434932da2c3be182e3d621
Johny Kim <johnyworld@naver.com>
2019-10-03 00:34:06 +0900

    Fix edit user api

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -4,14 +4,21 @@ export default {
     Mutation: {
         editUser: async(_, args, {request, isAuthenticated}) => {
             isAuthenticated(request);
-            const { username, fullname, bio, lang } = args;
+            const { id, username, fullname, bio, lang } = args;
             const { user } = request;
-            const target = await prisma.user({ username });
-            if ( target.id === user.id ) {
-                return prisma.updateUser({
-                    where: { username },
-                    data : { fullname, bio, lang }
+
+            const regUsername = /^[a-z0-9_-]{3,16}$/; 
+            const regFullname = /^[^0-9\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]{2,18}$/;
+
+            if ( !regUsername.test( username ) ) throw Error("You can make username by 3-15 characters, lower case, number, -, _");
+            if ( !regFullname.test( fullname ) ) throw Error("You can make fullname by 2-17 characters, only alphabets");
+
+            if ( id === user.id ) {
+                await prisma.updateUser({
+                    where: { id: user.id },
+                    data : { username, fullname, bio, lang }
                 }) 
+                return true;
             } else {
                 throw Error("It is not me.");
             }
