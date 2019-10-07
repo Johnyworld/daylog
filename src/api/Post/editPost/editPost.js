8a0e9612198398a66fbd02dde5e28989fdec63dc
Johny Kim <johnyworld@naver.com>
2019-10-07 17:14:07 +0900

    Fix edit post api

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,11 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
 
-const SCORE = "SCORE";
-const LOCATION = "LOCATION";
-const STARTAT = "STARTAT"
-const ENDAT = "ENDAT"
-const DOING = "DOING"
-
 export default {
     Mutation : {
         editPost: async(_, args, {request, isAuthenticated}) => {
@@ -15,11 +9,11 @@ export default {
             const post = await prisma.$exists.post({ id, user : { id: user.id } });
             if ( post ) {
                 switch(type) {
-                    case SCORE : return prisma.updatePost({ where: { id }, data: { score } });
-                    case LOCATION : return prisma.updatePost({ where: { id }, data: { location }});
-                    case STARTAT : return prisma.updatePost({ where: { id }, data: { startAt }});
-                    case ENDAT : return prisma.updatePost({ where: { id }, data: { endAt }}); 
-                    case DOING : return prisma.updatePost({ where: { id }, data: { doing : { connect : { id : doingId }}}}); 
+                    case "score" : return prisma.updatePost({ where: { id }, data: { score } });
+                    case "location" : return prisma.updatePost({ where: { id }, data: { location }});
+                    case "startAt" : return prisma.updatePost({ where: { id }, data: { startAt }});
+                    case "endAt" : return prisma.updatePost({ where: { id }, data: { endAt }}); 
+                    case "doing" : return prisma.updatePost({ where: { id }, data: { doing : { connect : { id : doingId }}}}); 
                 }
             } else {
                 throw Error("It is not your post.")
