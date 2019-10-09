39c67e8ca7d172e9adc44e9b26d01efb768abc05
Johny Kim <johnyworld@naver.com>
2019-10-09 20:49:34 +0900

    Add rule to create post

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -5,17 +5,38 @@ export default {
     Mutation : {
         upload: async(_, args, {request, isAuthenticated}) => {
             isAuthenticated(request);
-            const { doingId, location="", score } = args;
-            const { user } = request; 
-            const startAt = timeToBlock( new Date().getHours(), new Date().getMinutes() );
+            const { doingId, location="", startAt, score } = args;
+            const { user } = request;
+
+            // const startAt = timeToBlock( new Date().getHours(), new Date().getMinutes() );
             const yyyymmdd = getYyyymmdd( new Date().getFullYear(), new Date().getMonth(), new Date().getDate() );
-            const post = await prisma.createPost({ 
-                location, score, yyyymmdd,
-                startAt, endAt: startAt+1,
-                user: { connect : { id: user.id }},
-                doing: { connect : { id: doingId }}
+            const yesterday = getYyyymmdd( new Date().getFullYear(), new Date().getMonth(), new Date().getDate()-1 );
+            
+           console.log(startAt);
+            const postExists = await prisma.$exists.post({
+                OR : [
+                    {
+                        yyyymmdd, 
+                        endAt : startAt + 1
+                    },
+                    {
+                        yyyymmdd: yesterday,
+                        endAt : startAt + 97
+                    }
+                ]
             });
-            return post;
+
+            if ( !postExists ) {
+                const post = await prisma.createPost({ 
+                    location, score, yyyymmdd,
+                    startAt, endAt: startAt+1,
+                    user: { connect : { id: user.id }},
+                    doing: { connect : { id: doingId }}
+                });
+                return post;
+            } else {
+                throw Error('There is a post in same time!');
+            }
         }
     }
 }
