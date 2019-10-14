2a626ace5c82a984c48fd32e23fefa7a427be2c9
Johny Kim <johnyworld@naver.com>
2019-10-14 19:56:23 +0900

    Update search doing condition

--- src/api/Doing/searchDoing/searchDoing.js
+++ src/api/Doing/searchDoing/searchDoing.js
@@ -2,13 +2,21 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Query : {
-        searchDoing: (_, args) => {
-            const { term } = args;
+        searchDoing: (_, args, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const { user: { id }} = request;
+            const { term, category } = args;
             return prisma.doings({
                 where: {
-                    OR: [
-                        { name_contains: term },
-                        { category: { name_contains: term } }
+                    AND : [
+                        category && { category: { name: category } },
+                        {
+                            OR: [
+                                { name_contains: term },
+                                { category: { name_contains: term } }
+                            ]
+                        },
+                        { pins_none : { user : { id } } }
                     ]
                 }
             })
