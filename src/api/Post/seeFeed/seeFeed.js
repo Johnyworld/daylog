2d65ac0e5c921047d19b4a9b1724f1c092326c3c
Johny Kim <johnyworld@naver.com>
2019-10-24 23:35:00 +0900

    Update seeFeed resolver

--- src/api/Post/seeFeed/seeFeed.js
+++ src/api/Post/seeFeed/seeFeed.js
@@ -2,7 +2,7 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Query : {
-        seeFeedPost : async(_, {limit=10, offset=0}, {request, isAuthenticated}) => {
+        seeFeed : async(_, {limit=10, offset=0}, {request, isAuthenticated}) => {
             isAuthenticated(request);
             const { user } = request;
             const posts = await prisma.posts({
@@ -17,36 +17,6 @@ export default {
                 orderBy: "updatedAt_DESC"
             })
 
-            // const reviews = await prisma.reviews({
-            //     where: {
-            //         OR : [
-            //             { user: { followers_some: { id: user.id } } },
-            //             { user: { id: user.id }}
-            //         ]
-            //     },
-            //     first: offset + limit,
-            //     orderBy: "createdAt_DESC"
-            // })
-
-            // const all = [ ...posts, ...reviews ]
-            //     .sort((a, b) => a.createdAt > b.createdAt ? -1 : a.createdAt < b.createdAt ? 1 : 0 )
-            //     .slice( offset, offset + limit )
-            //     .map( item => ({ ...item, __typename: 'Feed' }));
-            
-            // let postsI = all.filter( item => item.startAt );
-            // let reviewsI = all.filter( item => !item.startAt );
-
-            return posts;
-            // return { 
-            //     posts: postsI,
-            //     reviews: reviewsI
-            // }
-        },
-
-        seeFeedReview : async(_, {limit=10, offset=0}, {request, isAuthenticated}) => {
-            isAuthenticated(request);
-            const { user } = request;
-
             const reviews = await prisma.reviews({
                 where: {
                     OR : [
@@ -54,11 +24,41 @@ export default {
                         { user: { id: user.id }}
                     ]
                 },
-                first: limit,
-                skip: offset,
+                first: offset + limit,
                 orderBy: "createdAt_DESC"
-            });
-            return reviews;
+            })
+
+            // const all = [ ...posts, ...reviews ]
+                // .sort((a, b) => a.createdAt > b.createdAt ? -1 : a.createdAt < b.createdAt ? 1 : 0 )
+                // .slice( offset, offset + limit )
+                // .map( item => ({ ...item, __typename: 'Feed' }));
+            
+            // let postsI = all.filter( item => item.startAt );
+            // let reviewsI = all.filter( item => !item.startAt );
+
+            // return posts;
+            return { 
+                posts: posts,
+                reviews: reviews
+            }
         }
+
+        // seeFeedReview : async(_, {limit=10, offset=0}, {request, isAuthenticated}) => {
+        //     isAuthenticated(request);
+        //     const { user } = request;
+
+        //     const reviews = await prisma.reviews({
+        //         where: {
+        //             OR : [
+        //                 { user: { followers_some: { id: user.id } } },
+        //                 { user: { id: user.id }}
+        //             ]
+        //         },
+        //         first: limit,
+        //         skip: offset,
+        //         orderBy: "createdAt_DESC"
+        //     });
+        //     return reviews;
+        // }
     }
 }
