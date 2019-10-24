43917c9a3e3d874caaf9a6fcc98eb0bebc67e65d
Johny Kim <johnyworld@naver.com>
2019-10-24 17:35:40 +0900

    Update Edit post resolver
    - pulling Timeblock to yesterday

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -1,5 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { getYyyymmdd } from "../../../utils";
+import { getToday, getYesterday } from "../../../utils";
 
 export default {
     Mutation : {
@@ -8,18 +8,37 @@ export default {
             const { id, doingId, location, score, startAt, endAt, type } = args;
             const { user } = request;
             const post = await prisma.$exists.post({ id, user : { id: user.id } });
+            const yyyymmdd = getToday();
+            const yesterday = getYesterday();
+
             if ( post ) {
                 switch(type) {
                     case "score" : return prisma.updatePost({ where: { id }, data: { score } });
                     case "location" : return prisma.updatePost({ where: { id }, data: { location }});
-                    case "startAt" : return prisma.updatePost({ where: { id }, data: { startAt, location: location && location }});
                     case "endAt" : return prisma.updatePost({ where: { id }, data: { endAt, location: location && location }}); 
                     case "doing" : return prisma.updatePost({ where: { id }, data: { doing : { connect : { id : doingId }}}}); 
                     case "delete" : return prisma.deletePost({ id });
-                    case "stEnYymd" : return prisma.updatePost({ where: { id }, data: { startAt, endAt }}); 
-                    case "stEnYymd_YesterToToday" :
-                        const yyyymmdd = getYyyymmdd( new Date().getFullYear(), new Date().getMonth(), new Date().getDate() );
-                        return prisma.updatePost({ where: { id }, data: { startAt, endAt, yyyymmdd }}); 
+                    
+                    case "pull" : return prisma.updatePost({ where: { id }, data: { 
+                        startAt, location: location && location 
+                    }});
+
+                    case "pullToYesterday" : return prisma.updatePost({ where: { id }, data: { 
+                        startAt,
+                        endAt,
+                        location: location && location,
+                        yyyymmdd: yesterday
+                    }});
+
+                    case "stEnYymd" : return prisma.updatePost({ where: { id }, data: { 
+                        startAt, endAt 
+                    }});
+
+                    case "stEnYymd_YesterToToday" : return prisma.updatePost({ where: { id }, data: { 
+                        startAt,
+                        endAt,
+                        yyyymmdd
+                    }}); 
                 }
             } else {
                 throw Error("It is not your post.")
